d8f34ac4cd34af855d99cab6149c060bc381a9d6
Vicente Nascimento <vicente19981@live.com>
2021-09-20 20:15:08 -0300

    feat: create wallet presentation screen

---
+++ src/presentation/screen/Wallet/wallet.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Wallet: React.FC = () => {
+  return <View />;
+}
+
+export default Wallet;
