73c55155c225f0e49902c4d07370d2585d18fb0c
Vicente Nascimento <vicente19981@live.com>
2021-09-21 12:41:13 -0300

    feat: content mocked done

--- src/presentation/screen/Content/content.tsx
+++ src/presentation/screen/Content/content.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import {Image, ScrollView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import Skelleton from '../../components/Skelleton';
+import {useContent} from '../../hooks/useContent';
 import {styles} from './styles';
 
 const Content: React.FC = () => {
+  const {filterSelected, loading, setFilterSelected} = useContent();
+
   return (
     <View style={styles.containerContent}>
       <View>
@@ -10,44 +14,53 @@ const Content: React.FC = () => {
           horizontal
           showsHorizontalScrollIndicator={false}
           style={styles.scrollViewContainerFilter}>
-          {[1, 2, 3, 4].map(post => {
-            const isSelected =
-              post === 1
-                ? styles.containerFilterSelected
-                : styles.containerFilterUnSelected;
+          {['todos', 'agronegócio', 'energia', 'real State'].map(
+            (filter, key) => {
+              const selectedBoxStyle =
+                filterSelected === filter
+                  ? styles.containerFilterSelected
+                  : styles.containerFilterUnSelected;
 
-            const isSelectedText =
-              post === 1
-                ? styles.textFilterSelected
-                : styles.textFilterUnSelected;
+              const selectedTextStyle =
+                filterSelected === filter
+                  ? styles.textFilterSelected
+                  : styles.textFilterUnSelected;
 
-            return (
-              <View style={[styles.containerFilter, isSelected]}>
-                <Text style={[styles.textFilter, isSelectedText]}>
-                  Tudo bem
-                </Text>
-              </View>
-            );
-          })}
+              return (
+                <TouchableOpacity
+                  key={key}
+                  onPress={() => setFilterSelected(filter)}
+                  style={[styles.containerFilter, selectedBoxStyle]}>
+                  <Text style={[styles.textFilter, selectedTextStyle]}>
+                    {filter}
+                  </Text>
+                </TouchableOpacity>
+              );
+            },
+          )}
         </ScrollView>
       </View>
-      <ScrollView
-        style={styles.scrolViewContainerPost}
-        contentContainerStyle={{paddingBottom: 32}}>
-        {[1, 2, 3, 4].map(post => (
-          <View style={styles.containerPost}>
-            <Image
-              style={styles.imagePost}
-              source={{
-                uri: 'https://silvas.pt/wp-content/uploads/2019/05/DSCF0400.jpg',
-              }}
-            />
-            <Text style={styles.textPost}>
-              IPO: oque sua empresa precisa saber antes de abrir o capital
-            </Text>
-          </View>
-        ))}
-      </ScrollView>
+      {loading ? (
+        <ScrollView
+          style={styles.scrolViewContainerPost}
+          contentContainerStyle={{paddingBottom: 32}}>
+          {[1, 2, 3, 4].map(post => (
+            <View style={styles.containerPost}>
+              <Image
+                style={styles.imagePost}
+                source={{
+                  uri: 'https://silvas.pt/wp-content/uploads/2019/05/DSCF0400.jpg',
+                }}
+              />
+              <Text style={styles.textPost}>
+                IPO: oque sua empresa precisa saber antes de abrir o capital
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      ) : (
+        <Skelleton />
+      )}
     </View>
   );
 };
