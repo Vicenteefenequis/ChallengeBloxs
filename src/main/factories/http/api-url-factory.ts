19ca29d30be3aaab0712a47d8059bff04a3ae388
Vicente Nascimento <vicente19981@live.com>
2021-09-21 19:42:07 -0300

    feat: create new link of api categories

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -2,3 +2,6 @@ export const makeApiUrl = (path?: string, page?: string): string =>
   path
     ? `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories=${path}&per_page=5&page=${page}`
     : `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories&per_page=5&page=${page}`;
+
+export const makeApiCategories = (): string =>
+  `https://conteudos.bloxs.com.br/wp-json/wp/v2/categories`;
