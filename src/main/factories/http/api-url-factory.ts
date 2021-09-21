2bf62b6b89c0fb25df05f6534aaad9af94c9a38b
Vicente Nascimento <vicente19981@live.com>
2021-09-21 17:09:01 -0300

    refactor: change api url maked

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,4 +1,4 @@
-export const makeApiUrl = (path?: string): string =>
+export const makeApiUrl = (path?: string, page?: string): string =>
   path
-    ? `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories=${path}`
-    : `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories`;
+    ? `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories=${path}&per_page=5&page=${page}`
+    : `https://conteudos.bloxs.com.br/wp-json/wp/v2/posts?_embed=1&categories&per_page=5&page=${page}`;
