baee09574540dd1d73f6c138afaf040a0262c603
Vicente Nascimento <vicente19981@live.com>
2021-09-20 23:34:08 -0300

    refactor: change names of colors and add news color dark

--- src/presentation/styles/globalColors.ts
+++ src/presentation/styles/globalColors.ts
@@ -1,6 +1,7 @@
 export default {
-    primaryColor: '#121212',
-    yellow100: "#FEC70A",
-    white: "#F2F2F2",
-    gray: "#8E8E8E"
- }
+  primaryColor: '#121212',
+  dark: '#1E1E1E',
+  yellow: '#FEC70A',
+  white: '#F2F2F2',
+  gray: '#8E8E8E',
+};
