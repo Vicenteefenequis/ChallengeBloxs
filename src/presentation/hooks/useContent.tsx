bd9dab320efe0b263f5b8f06840f69e52945ee7e
Vicente Nascimento <vicente19981@live.com>
2021-09-21 21:38:42 -0300

    feat: added categories list and set

--- src/presentation/hooks/useContent.tsx
+++ src/presentation/hooks/useContent.tsx
@@ -6,12 +6,14 @@ import React, {
   useState,
 } from 'react';
 import {ContentModel} from '../../domain/models';
+import {CategoryModel} from '../../domain/models/category-model';
 import {LoadContentList} from '../../domain/usecases';
-import {FilterType} from '../utils/FilterTypeParameter';
+import {LoadCategoryList} from '../../domain/usecases/load-category-list';
 
 type ContentContextProps = {
   children: React.ReactNode;
   content: (path?: string, page?: string) => LoadContentList;
+  category: LoadCategoryList;
 };
 
 type ContentContext = {
@@ -22,24 +24,30 @@ type ContentContext = {
   setPage: React.Dispatch<React.SetStateAction<number>>;
   loadingMoreContent: boolean;
   handleMoreContent: (page: number, filterSelected: string) => Promise<void>;
+  categoryList: CategoryModel[];
 };
 
 export const contentContext = createContext<ContentContext>(
   {} as ContentContext,
 );
 
-const ContentContext = ({children, content}: ContentContextProps) => {
-  const [filterSelected, setFilterSelected] = useState('todos');
+const ContentContext = ({children, content, category}: ContentContextProps) => {
+  const categoryMap = new Map<any, any>();
+
+  const [filterSelected, setFilterSelected] = useState('Todos');
   const [contentList, setContentList] = useState<ContentModel[]>([]);
   const [page, setPage] = useState(1);
   const [loadingMoreContent, setLoadingMoreContent] = useState(false);
+  const [categoryList, setCategoryList] = useState<CategoryModel[]>(
+    [] as CategoryModel[],
+  );
 
   const handleMoreContent = useCallback(
-    async (page: number, filterSelected: string) => {
+    async (page: number, filter: string) => {
       setLoadingMoreContent(true);
 
       const allContent = await content(
-        FilterType.get(filterSelected),
+        categoryMap.get(filter),
         page.toString(),
       ).loadContent();
 
@@ -51,7 +59,14 @@ const ContentContext = ({children, content}: ContentContextProps) => {
 
   useEffect(() => {
     handleMoreContent(page, filterSelected);
-  }, [filterSelected, page]);
+  }, [page, filterSelected]);
+
+  useEffect(() => {
+    category.loadCategory().then(categories => {
+      categories.map(ctg => categoryMap.set(ctg.name, ctg.id));
+      setCategoryList([{id: 0, name: 'Todos'}, ...categories]);
+    });
+  }, []);
 
   return (
     <contentContext.Provider
@@ -63,6 +78,7 @@ const ContentContext = ({children, content}: ContentContextProps) => {
         setPage,
         loadingMoreContent,
         handleMoreContent,
+        categoryList,
       }}>
       {children}
     </contentContext.Provider>
