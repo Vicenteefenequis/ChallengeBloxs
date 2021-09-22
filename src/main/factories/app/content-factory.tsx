1591934859451fab7f29aa5ddea16978b313c03e
Vicente Nascimento <vicente19981@live.com>
2021-09-21 21:37:10 -0300

    feat: passing category make load in context

--- src/main/factories/app/content-factory.tsx
+++ src/main/factories/app/content-factory.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ContentContext from '../../../presentation/hooks/useContent';
 import Content from '../../../presentation/screen/Content/content';
+import {makeRemoteLoadCategoryList} from '../usecases/load-category-list/remote-load-category-list';
 import {makeRemoteLoadContentList} from '../usecases/load-content-list/remote-load-content-list';
 
 export const makeContent: React.FC = () => {
   return (
-    <ContentContext content={makeRemoteLoadContentList}>
+    <ContentContext
+      content={makeRemoteLoadContentList}
+      category={makeRemoteLoadCategoryList()}>
       <Content />
     </ContentContext>
   );
